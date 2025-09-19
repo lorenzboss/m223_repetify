b719352280300f1a3ccf83f3cf97f5100e36ee03
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-09-19 14:29:52 +0200

    Add new clean notices and alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :authenticate_user!
+
+  private
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 end

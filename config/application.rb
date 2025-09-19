0d18310febff0e1b6c743388803a87fd39698a4f
Lorenz Boss <lorenz.boss@bbzbl-it.ch>
2025-09-20 01:00:11 +0200

    Set time zone to Bern in application configuration

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module M223Repetify
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Bern"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
